96d9b5a6b81207889d6162d49897c43bc9adc404
Gary Josack <gary@byoteki.com>
2021-06-28 22:20:11 -0700

    Poison Run v1.1
    * Pets now save you

--- Poison Run/main.lua
+++ Poison Run/main.lua
@@ -1,5 +1,5 @@
 meta.name = "Poison Run"
-meta.version = "1"
+meta.version = "1.1"
 meta.description = "You're always poisoned. Can you survive?"
 meta.author = "garebear"
 
@@ -7,21 +7,79 @@ register_option_int(
     "poison_ticks",
     "Number of game ticks until you're damaged from poison.",
     1800,
-    0, 18000
+    0,
+    18000
 )
 
-set_callback(function()
+
+DEBUG = false
+
+function debug(msg)
+    if DEBUG then
+        message(tostring(msg))
+    end
+end
+
+
+function clear_poison()
     for i, player in ipairs(players) do
-        if player.health ~= 0 then
+        if player.health ~= 0 and player:is_poisoned() then
+            debug("Clearing poison on player " .. tostring(i))
             player:poison(-1)
         end
     end
-end, ON.LEVEL)
+end
 
-set_callback(function()
+
+function set_poison()
     for i, player in ipairs(players) do
         if player.health ~= 0 and not player:is_poisoned() then
+            debug("Setting poison on player " .. tostring(i))
             player:poison(options.poison_ticks)
         end
+    end 
+end
+
+
+function increment_health()
+    for i, player in ipairs(players) do
+        if player.health ~= 0 then
+            debug("Giving health to player " .. tostring(i))
+            player.health = player.health + 1
+        end
+    end 
+end
+
+
+function pets_saved()
+    saved = state.saved_dogs + state.saved_cats + state.saved_hamsters
+    debug("Pets saved: " .. tostring(saved))
+    return saved
+end
+
+
+LAST_PETS_SAVED = 0
+
+set_callback(function()
+    debug("Starting new run")
+    LAST_PETS_SAVED = 0
+end, ON.START)
+
+
+set_callback(function()
+    current_saved = pets_saved()
+    if current_saved > LAST_PETS_SAVED then
+        increment_health()
     end
+    LAST_PETS_SAVED = current_saved
+end, ON.TRANSITION)
+
+
+set_callback(function()
+    clear_poison()
+end, ON.LEVEL)
+
+
+set_callback(function()
+    set_poison()
 end, ON.FRAME)
